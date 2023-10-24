68b77ae7c91e917c2dc3447d29171bd5aa415795
Alejandro <alejandroisa71@gmail.com>
2023-10-24 02:41:56 -0300

    Create page results

---
+++ src/components/ResultCard.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const ResultCard = ({ result }) => {
+  console.log(result);
+  const { date } = result;
+  return (
+    <div className="flex justify-around  hover:bg-orange-400 md:text-2xl">
+        <p>
+          {result.id}. {result.date}
+        </p>
+        <Link href="/01-09-2023.pdf" target="_blank" download="01-09-2023.pdf">
+          Download
+        </Link>
+    </div>
+  );
+};
+
+export default ResultCard;
