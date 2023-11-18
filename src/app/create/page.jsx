1c641d5ac14fc351faa8bc0a7a409523d9b4de9e
nicojpalacio <nicojpalacio@gmail.com>
2023-11-18 01:18:01 -0300

    added-select-routines-feature

--- src/app/create/page.jsx
+++ src/app/create/page.jsx
@@ -1,49 +1,91 @@
 'use client'
-import Link from "next/link";
-import Select from "react-select";
-
-
-const optionsBodyParts = [
-  { value: "arms", label: "Arms" },
-  { value: "legs", label: "Legs" },
-  { value: "chest", label: "Chest" },
-  
-];
-
-
-const optionsObjectives = [
-  { value: "strength", label: "Strength" },
-  { value: "volume", label: "Volume" },
-  { value: "endurance", label: "Endurance" },
- 
-];
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import Select from 'react-select';
+import axios from 'axios';
 
 const InputView = () => {
+  const [bodyPartsOptions, setBodyPartsOptions] = useState([]);
+  const [objectivesOptions, setObjectivesOptions] = useState([]);
+  const [selectedBodyPart, setSelectedBodyPart] = useState(null);
+  const [selectedObjective, setSelectedObjective] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Llamada a la API para obtener las opciones de bodyParts
+        const bodyPartsResponse = await axios.get('/api/body-parts'); // Ajusta la URL de la API según tu configuración
+        const bodyPartsMappedOptions = bodyPartsResponse.data.map(item => ({
+          value: item.name,
+          label: item.name,
+        }));
+        console.log('Mapped Body Parts Options:', bodyPartsMappedOptions);
+        setBodyPartsOptions(bodyPartsMappedOptions);
+  
+        // Llamada a la API para obtener las opciones de objectives
+        const objectivesResponse = await axios.get('/api/objetives'); // Ajusta la URL de la API según tu configuración
+        const objectivesMappedOptions = objectivesResponse.data.map(item => ({
+          value: item.description,
+          label: item.description,
+        }));
+        console.log('Mapped Objectives Options:', objectivesMappedOptions);
+        setObjectivesOptions(objectivesMappedOptions);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+
+  const handleBodyPartChange = (selectedOption) => {
+    setSelectedBodyPart(selectedOption);
+  };
+
+  const handleObjectiveChange = (selectedOption) => {
+    setSelectedObjective(selectedOption);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Crear un objeto con los valores seleccionados
+    const selectedValues = {
+      bodyPart: selectedBodyPart,
+      objective: selectedObjective,
+    };
+
+    // Mostrar el objeto por consola
+    console.log('Selected Values:', selectedValues);
+  };
+
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-900 text-white">
       <div className="text-center mb-6">
         <h1 className="text-zinc-50 text-xl font-extrabold">CREATE YOUR PLAN</h1>
         <div className="bg-appOrange px-4 py-2 rounded-full">Select your options</div>
       </div>
-      <form className="w-full max-w-md">
+      <form className="w-full max-w-md" onSubmit={handleSubmit}>
         <div className="mb-4">
           <label className="block text-white font-bold mb-2">Body parts</label>
           <Select
-            options={optionsBodyParts}
+            options={bodyPartsOptions}
             className="w-full text-black"
             placeholder="Select Body Part"
+            onChange={handleBodyPartChange}
           />
         </div>
         <div className="mb-6">
           <label className="block text-white font-bold mb-2">Objective</label>
           <Select
-            options={optionsObjectives}
+            options={objectivesOptions}
             className="w-full text-black"
             placeholder="Select Objective"
+            onChange={handleObjectiveChange}
           />
         </div>
         <div className="flex justify-center items-center">
-          <button className="rounded-full bg-appOrange text-gray-50 text-3xl font-semibold px-6 py-2">
+          <button className="rounded-full bg-appOrange text-gray-50 text-3xl font-semibold px-6 py-2" type="submit">
             Submit
           </button>
         </div>
@@ -56,3 +98,4 @@ const InputView = () => {
 };
 
 export default InputView;
+
