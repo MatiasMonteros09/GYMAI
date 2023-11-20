0e12995461606d955b9f673646020b5e34bfb3d2
MatiasMonteros09 <matiasmonteros52@gmail.com>
2023-11-20 20:50:42 -0300

    Modified HomePage

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,52 @@
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram, faWhatsapp, faXTwitter } from "@fortawesome/free-brands-svg-icons";
+
 
 const HomePage = () => {
   return (
-    <div>
-     <h1>Home Page</h1>
-    </div>
+    <section>
+      <div className="content-center">
+        <img src="https://www.palco23.com/thumb/MTIwMHg2NzV4MXgxLjAuMTEw/files/2020/19_redaccion/fitness/the+gym/the-gym-948.jpg"
+          className="w-full h-auto"></img>
+        <h1 className="text-center text-zinc-50 text-xl font-extrabold p-5">
+          GYM <span className="text-appOrange text-xl font-extrabold">AI</span></h1>
+      </div>
+      <main className="bg-appOrange py-5">
+        <h2 className="text-center text-black text-xl font-extrabold">WE CREATE YOUR PLAN</h2>
+      </main>
+      <aside className="flex justify-around p-5">
+        <div className="flex text-blue-700 h-10 ml-5">
+          <FontAwesomeIcon icon={faFacebook} />
+        </div>
+        <div className="flex text-fuchsia-800 h-10 ml-5">
+          <FontAwesomeIcon icon={faInstagram} />
+        </div>
+        <div className="flex text-lime-600 h-10 ml-5">
+          <FontAwesomeIcon icon={faWhatsapp} />
+        </div>
+        <div className="flex text-gray-950 h-10 ml-5">
+          <FontAwesomeIcon icon={faXTwitter} />
+        </div>
+      </aside>
+      <div className="bg-appOrange p-5">
+        <h3 className="text-center text-zinc-50 text-xl font-extrabold">
+          CONTACT <span className="text-black text-xl font-extrabold">US</span>
+        </h3>
+      </div>
+      <footer className="flex flex-col items-center text-xl font-extrabold p-5">
+        <h3>LET'S TRAIN</h3>
+        <h3 className="text-appOrange text-xl font-extrabold">TOGETHER</h3>
+        <div className="py-5">
+          <button className="rounded-lg bg-appOrange p-5 mx-3">
+            <Link href="/signup">SIGN UP</Link>
+          </button>
+          <button className="rounded-lg bg-appOrange p-5 mx-3">
+            <Link href="/authview">SIGN IN</Link>
+          </button>
+        </div>
+      </footer>
+    </section>
   );
 };
 export default HomePage;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 const Navbar = async () => {
   const session = await getServerSession(authOptions);
-  
+
 
   return (
     <div className=" bg-appOrange">
@@ -15,43 +15,43 @@ const Navbar = async () => {
         <HamburgerNav />
       </div>
       <div>
-        <nav className="hidden bg-appOrange mb-4 lg:flex lg:justify-between lg:items-center px-20 py-3 font-bold">
+        <nav className="hidden bg-appOrange lg:flex lg:justify-between lg:items-center px-20 py-3 font-bold">
 
           <Link href="/"> Home</Link>
           <ul className="lg:flex gap-5">
-            {!session?.user ?(
-           <>
-            <li>
-              <Link href="/signup"> Sign Up</Link>
-            </li>
-            <li>
-              <Link href="/authview">Sign In</Link>
-            </li>
-            
-           </>
-              ):(
+            {!session?.user ? (
               <>
-              <li>  
-              <Link href="/downloadview"> Download Plan</Link>
-            </li>
-            <li>
-              <Link href="/create"> Create Plan</Link>
-            </li>
-            <li>
-              <Link href="/results"> Results</Link>
-            </li>
-            <li>
-              <Logout />
-            </li>
-            {session.user.name}
-            </>
+                <li>
+                  <Link href="/signup"> Sign Up</Link>
+                </li>
+                <li>
+                  <Link href="/authview">Sign In</Link>
+                </li>
+
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link href="/downloadview"> Download Plan</Link>
+                </li>
+                <li>
+                  <Link href="/create"> Create Plan</Link>
+                </li>
+                <li>
+                  <Link href="/results"> Results</Link>
+                </li>
+                <li>
+                  <Logout />
+                </li>
+                {session.user.name}
+              </>
             )}
-            
+
           </ul>
         </nav>
       </div>
     </div>
   );
-  
+
 };
 export default Navbar;
