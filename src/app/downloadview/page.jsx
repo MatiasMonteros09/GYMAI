7f5c4eb4f5f24ad8dd18fcb909255df7dcc7f94b
nicojpalacio <nicojpalacio@gmail.com>
2023-12-02 03:59:39 -0300

    addedSpinnerAndModal

--- src/app/downloadview/page.jsx
+++ src/app/downloadview/page.jsx
@@ -5,12 +5,15 @@ import Link from "next/link";
 import React from "react";
 import { BsFiletypePdf } from "react-icons/bs";
 import "./page.css";
-
+import {useSession} from "next-auth/react";
 import useStore from "@/app/store/selectroutine";
 
 import { useEffect, useState } from "react";
 import { obtenerRespuesta } from "@/components/ChatGpt";
-
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Spinner from "react-bootstrap/Spinner"; // Importa el componente Spinner
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Downloadview = () => {
   const { selectedValues } = useStore();
@@ -18,6 +21,12 @@ const Downloadview = () => {
   const [respuesta, setRespuesta] = useState("");
 
   const apiUrl = "api/routine";  // Reemplaza con la URL real de tu API
+  const {data:session} =  useSession();
+  const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(true); // Nuevo estado para manejar la carga
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
 
 const sendResponseToPrisma = async (data) => {
   try {
@@ -41,12 +50,14 @@ const sendResponseToPrisma = async (data) => {
 
   
 useEffect(() => {
+  setLoading(true);
   if (selectedValues?.bodyPart?.label && selectedValues?.objective?.label) {
     obtenerRespuesta(selectedValues.bodyPart.label, selectedValues.objective.label)
       .then((res) => {
         setRespuesta(res);
         // Enviar la respuesta a la tabla Routine
-        sendResponseToPrisma({ response:res, creatorId: 1 /* reemplazar con el ID real */, /* otros campos */ });
+        sendResponseToPrisma({ response:res, creatorId: session?.user?.id /* reemplazar con el ID real */, /* otros campos */ });
+        setLoading(false);
       });
   }
 }, [selectedValues]);
@@ -64,15 +75,31 @@ useEffect(() => {
       <div className=" w-screen plan-container bg-appOrange px-5  mb-20 flex flex-col justify-around items-center">
         <h1 className="font-extrabold text-xl text-zinc-50">MAKE THIS PLAN</h1>
 
-        <h1 className="font-extrabold text-xl text-slate-800">
-          {selectedValues.bodyPart && selectedValues.bodyPart.label}
-        </h1>
-        <h1 className="font-extrabold text-xl text-slate-800">
-          {selectedValues.objective && selectedValues.objective.label}
-        </h1>
+       
         <div>
-          
-        <p>Respuesta: {respuesta}</p>
+          <Button variant="secondary" onClick={handleShow}>
+            Routine
+          </Button>
+
+          <Modal className="text-black" show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Making the best routine 4U</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              {loading ? ( // Muestra el spinner mientras se carga
+                <Spinner animation="border" role="status">
+                  <span className="text-black visually-hidden">Loading...</span>
+                </Spinner>
+              ) : (
+                <p>Routine: {respuesta}</p>
+              )}
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
         </div>
 
         <h1 className="font-extrabold text-xl text-slate-800">FOR YOU</h1>
