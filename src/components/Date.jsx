d57945358ea3acf92090fbea7d0a8a17e8e84b7c
Alejandro <alejandroisa71@gmail.com>
2023-12-17 19:36:49 -0300

    Other Change styled button and link homepage

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -5,5 +5,7 @@ import { parseISO, format } from "date-fns";
 export default function Date({ dateString }) {
   const date = parseISO(dateString);
 
-  return <time>{format(date, `dd/MM/yyyy`)}</time>;
+  return (
+  <time>{format(date, `dd/MM/yyyy`)}</time>
+  )
 }
