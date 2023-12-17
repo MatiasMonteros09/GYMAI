33bb1340531c8921938b1dc90fe6684f86f2d1de
MatiasMonteros09 <matiasmonteros52@gmail.com>
2023-12-17 17:40:20 -0300

    modify style homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,18 +1,18 @@
 import Link from 'next/link';
- // Corrige la ruta según la estructura de tu proyecto
+// Corrige la ruta según la estructura de tu proyecto
 
 const HomePage = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
       <div className="text-center">
-        
+
         <h1 className="text-3xl md:text-5xl font-extrabold mb-2">Welcome to GYM AI</h1>
         <p className="text-base md:text-lg mb-4">Your Personalized Workout Planning Tool</p>
       </div>
 
       <div className="mt-8 space-y-4 w-full flex items-center justify-center">
         <Link href="/signin" className="text-zinc-50 text-2xl px-6 py-4 mb-4 mt-3 font-semibold rounded-lg bg-appOrangeButton hover:shadow-inner transform hover:scale-110 hover:bg-opacity-50 transition ease-out duration-300">
-            Get Started
+          Get Started
         </Link>
       </div>
     </div>
